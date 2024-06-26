1825e54a7211af446f96a7451a7afcb4cbeadf29
Zhaoshan-Duan <joshua.duan47@gmail.com>
2024-06-26 16:44:17 -0700

    update project

--- app/src/main/java/org/jojo/dessert_pusher/MainActivity.kt
+++ app/src/main/java/org/jojo/dessert_pusher/MainActivity.kt
@@ -1,9 +1,14 @@
 package org.jojo.dessert_pusher
 
+import android.content.ActivityNotFoundException
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ShareCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.databinding.DataBindingUtil
@@ -15,33 +20,117 @@ class MainActivity : AppCompatActivity() {
     private var revenue = 0
     private var dessertsSold = 0
 
-    data class Dessert(val imageId: Int, val price: Int, val startProductionAmount: Int)
 
-    private val allDesserts = listOf<Dessert>()
-   // private var currentDessert = allDesserts.first()
+        data class Dessert(val imageId: Int, val price: Int, val startProductionAmount: Int)
 
+        private val allDesserts = listOf(
+            Dessert(R.drawable.cupcake, 5, 0),
+            Dessert(R.drawable.donut, 10, 5),
+            Dessert(R.drawable.eclair, 15, 20),
+            Dessert(R.drawable.froyo, 30, 50),
+            Dessert(R.drawable.gingerbread, 50, 100),
+            Dessert(R.drawable.honeycomb, 100, 200),
+            Dessert(R.drawable.icecreamsandwich, 500, 500),
+            Dessert(R.drawable.jellybean, 1000, 1000),
+            Dessert(R.drawable.kitkat, 2000, 2000),
+            Dessert(R.drawable.lollipop, 3000, 4000),
+            Dessert(R.drawable.marshmallow, 4000, 8000),
+            Dessert(R.drawable.nougat, 5000, 16000),
+            Dessert(R.drawable.oreo, 6000, 20000)
+        )
+        private var currentDessert = allDesserts[0]
 
-
-    private lateinit var binding: ActivityMainBinding
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        Timber.i("onCreate called")
+        private lateinit var binding: ActivityMainBinding
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            Timber.i("onCreate called")
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         binding.dessertButton.setOnClickListener {
-            //onDessertClicked()
+            onDessertClicked()
         }
 
         binding.revenue = revenue
         binding.amountSold = dessertsSold
 
-    //    binding.dessertButton.setImageResource(currentDessert.imageId)
+        binding.dessertButton.setImageResource(currentDessert.imageId)
+        }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
 
+    private fun onDessertClicked() {
+        revenue += currentDessert.price
+        dessertsSold++
+
+        binding.revenue = revenue
+        binding.amountSold = dessertsSold
+
+        showCurrentDessert()
+    }
+
+    private fun showCurrentDessert() {
+       var newDessert = allDesserts[0]
+        for (dessert in allDesserts) {
+            if (dessertsSold >= dessert.startProductionAmount) newDessert = dessert
+            else break
+        }
+        if (newDessert != currentDessert) {
+            currentDessert = newDessert
+            binding.dessertButton.setImageResource(newDessert.imageId)
+        }
+    }
+
+    private fun onShare() {
+        val shareIntent = ShareCompat.IntentBuilder.from(this)
+            .setText(getString(R.string.share_text, dessertsSold, revenue))
+            .setType("text/plain").intent
+        try {
+            startActivity(shareIntent)
+        } catch (ex: ActivityNotFoundException) {
+            Toast.makeText(this, getString(R.string.sharing_not_available), Toast.LENGTH_LONG)
+                .show()
+        }
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.shareMenuButton -> onShare()
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     override fun onStart() {
         super.onStart()
         Timber.i("onStart called")
     }
+
+    override fun onResume() {
+        super.onResume()
+        Timber.i("onResume called")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Timber.i("onRestart called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Timber.i("onPause called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Timber.i("onStop called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Timber.i("onDestroy called")
+    }
+
 }
